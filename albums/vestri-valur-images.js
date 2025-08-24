1371e9915da0f69e41c3c6a7d147b32159c1d679
Guðmundur Þórlaugarson <gudmundur27@gmail.com>
2025-08-24 01:17:44 +0000

    Add Vestri - Valur album with dynamic image rendering; update index and gallery pages

---
+++ albums/vestri-valur-images.js
@@ -0,0 +1,301 @@
+const vestriValurImages = [
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994670/Vestri-Valur-01_ghinfocus_ykgku2.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994674/Vestri-Valur-02_ghinfocus_msvchh.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994674/Vestri-Valur-03_ghinfocus_lawmgt.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994675/Vestri-Valur-04_ghinfocus_pohykw.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994676/Vestri-Valur-05_ghinfocus_y9jlwu.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994679/Vestri-Valur-06_ghinfocus_dbe0wy.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994679/Vestri-Valur-07_ghinfocus_eopycb.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994679/Vestri-Valur-08_ghinfocus_myk8md.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994680/Vestri-Valur-09_ghinfocus_tgk8tp.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994683/Vestri-Valur-10_ghinfocus_t4clll.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994683/Vestri-Valur-11_ghinfocus_qqzalz.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994683/Vestri-Valur-12_ghinfocus_mvxhoo.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994684/Vestri-Valur-13_ghinfocus_frc8sb.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994686/Vestri-Valur-14_ghinfocus_llwght.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994446/Vestri-Valur-15_ghinfocus_ny4jkz.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994446/Vestri-Valur-16_ghinfocus_uplj5f.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994666/Vestri-Valur-Landscape-01_ghinfocus_hgzvlz.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994446/Vestri-Valur-17_ghinfocus_h8ygix.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994446/Vestri-Valur-18_ghinfocus_ozwcnx.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994447/Vestri-Valur-19_ghinfocus_fcuiqr.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994448/Vestri-Valur-20_ghinfocus_fibakx.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994447/Vestri-Valur-21_ghinfocus_b984nc.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994447/Vestri-Valur-22_ghinfocus_ekiyua.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994448/Vestri-Valur-23_ghinfocus_bcsh48.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994449/Vestri-Valur-24_ghinfocus_iz3ssj.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994449/Vestri-Valur-25_ghinfocus_ql2fze.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994450/Vestri-Valur-26_ghinfocus_h4ltyc.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994450/Vestri-Valur-27_ghinfocus_p4oliw.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994451/Vestri-Valur-28_ghinfocus_b84n6l.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994452/Vestri-Valur-29_ghinfocus_zcfd6j.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994452/Vestri-Valur-30_ghinfocus_va5bg7.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994453/Vestri-Valur-31_ghinfocus_zdatvc.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994454/Vestri-Valur-32_ghinfocus_zpbnga.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994454/Vestri-Valur-33_ghinfocus_lq3lpj.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994455/Vestri-Valur-34_ghinfocus_ezya4p.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994455/Vestri-Valur-35_ghinfocus_frayhq.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994456/Vestri-Valur-36_ghinfocus_l0ji0w.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994457/Vestri-Valur-37_ghinfocus_gy4ceu.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994458/Vestri-Valur-38_ghinfocus_krfgth.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994458/Vestri-Valur-39_ghinfocus_o29ggz.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994459/Vestri-Valur-40_ghinfocus_blfw0q.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994460/Vestri-Valur-41_ghinfocus_co5kwe.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994460/Vestri-Valur-42_ghinfocus_ma5bqw.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994461/Vestri-Valur-43_ghinfocus_n23lrv.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994461/Vestri-Valur-44_ghinfocus_s89d8w.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994462/Vestri-Valur-45_ghinfocus_wxbvui.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994463/Vestri-Valur-46_ghinfocus_ue5o8u.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994463/Vestri-Valur-47_ghinfocus_lpaegw.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994464/Vestri-Valur-48_ghinfocus_uo07tn.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994465/Vestri-Valur-49_ghinfocus_ciu1t9.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994465/Vestri-Valur-50_ghinfocus_dzd51p.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994465/Vestri-Valur-51_ghinfocus_hvtcjc.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994467/Vestri-Valur-52_ghinfocus_badhdz.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994467/Vestri-Valur-53_ghinfocus_m4uwh8.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994468/Vestri-Valur-54_ghinfocus_chodmn.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994468/Vestri-Valur-55_ghinfocus_pcijyg.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994470/Vestri-Valur-56_ghinfocus_usidzg.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994470/Vestri-Valur-57_ghinfocus_eiknne.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994471/Vestri-Valur-58_ghinfocus_mm23fv.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994471/Vestri-Valur-59_ghinfocus_gz7zph.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994472/Vestri-Valur-60_ghinfocus_ni3qne.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994473/Vestri-Valur-61_ghinfocus_ndzxoe.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994487/Vestri-Valur-62_ghinfocus_dcr7k7.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994515/Vestri-Valur-63_ghinfocus_cl9yad.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994543/Vestri-Valur-64_ghinfocus_gf4fx1.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994570/Vestri-Valur-65_ghinfocus_hnv4fk.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994596/Vestri-Valur-66_ghinfocus_yuyyfv.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994623/Vestri-Valur-67_ghinfocus_cyjhi9.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994640/Vestri-Valur-68_ghinfocus_adbvm8.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994641/Vestri-Valur-69_ghinfocus_dakt5m.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994641/Vestri-Valur-70_ghinfocus_lkrde6.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994641/Vestri-Valur-71_ghinfocus_h1vlcz.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994642/Vestri-Valur-72_ghinfocus_bckyy2.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994644/Vestri-Valur-73_ghinfocus_bdyocq.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994644/Vestri-Valur-74_ghinfocus_akeoay.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994645/Vestri-Valur-75_ghinfocus_dwrveo.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994646/Vestri-Valur-76_ghinfocus_fhb25o.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994650/Vestri-Valur-77_ghinfocus_npcbv9.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994650/Vestri-Valur-78_ghinfocus_ftigrq.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994650/Vestri-Valur-79_ghinfocus_igdcwb.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994669/Vestri-Valur-Landscape-02_ghinfocus_khorro.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994651/Vestri-Valur-80_ghinfocus_dfw4ms.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994654/Vestri-Valur-81_ghinfocus_tv7cqi.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994654/Vestri-Valur-82_ghinfocus_cijh86.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994654/Vestri-Valur-83_ghinfocus_jcjmt2.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994654/Vestri-Valur-84_ghinfocus_jetdja.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994657/Vestri-Valur-85_ghinfocus_cap4xk.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994658/Vestri-Valur-86_ghinfocus_pe6ba7.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994658/Vestri-Valur-87_ghinfocus_wu43ff.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994658/Vestri-Valur-88_ghinfocus_ywuqze.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994661/Vestri-Valur-89_ghinfocus_grqrx4.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994662/Vestri-Valur-90_ghinfocus_q21lva.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994662/Vestri-Valur-91_ghinfocus_lvhwvn.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994665/Vestri-Valur-92_ghinfocus_qz8ynf.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994670/Vestri-Valur-Landscape-03_ghinfocus_glphmj.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994665/Vestri-Valur-93_ghinfocus_cvegue.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994665/Vestri-Valur-94_ghinfocus_otfzpm.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994670/Vestri-Valur-Landscape-04_ghinfocus_to0xzf.jpg",
+  },
+  {
+    url: "https://res.cloudinary.com/dfb5syv1z/image/upload/v1755994665/Vestri-Valur-95_ghinfocus_waawp0.jpg",
+  },
+];
+
+window.vestriValurImages = vestriValurImages;
